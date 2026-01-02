b4f6e3162a7604cccd6ff9da924d42a588725716
ST9-DEV <stanislawjurek.ina@gmail.com>
2026-01-02 07:47:06 +0100

    "MathuRPG" WIP (ver. 0.03)

--- MathuRPG/model/math_problems/tasks_generators.py
+++ MathuRPG/model/math_problems/tasks_generators.py
@@ -1,6 +1,6 @@
 import random as r
 
-TASK_TYPES = ["arithmetic", "equation", "geometry", "square function", "trigonometry", None, None, None, None, None] # lista możliwych typów zadań
+TASK_TYPES = ["arithmetic", "basic equation", "geometry", "square function", "trigonometry"] # lista możliwych typów zadań
 OPERATORS = ["+", "-", "*", ":"] #lista operatorów
 
 # zakres poziomów [1, 3] - trzy poziomy trudności
@@ -14,32 +14,25 @@ def operator_before_value(val):
     if val < 0:
         op = OPERATORS[1] # minus
         val = str(abs(val)) # uniknięcie podwójnego minusa w zapisie, który nie spełnia równiania
-    else: op = OPERATORS[0] # plus
+    else: 
+        op = OPERATORS[0] # plus
 
     return op, val
 
 #funkcja do redukcji w zapisie jeśli 1 lub -1 występuje przed "x"
 def val_next_to_x(val):
-    if val == 1:
-        str_val = ""
-    elif val == -1:
-        str_val = "-"
-    else: str_val = str(val)
-
-    return str_val
+    return "" if val == 1 else "-" if val == -1 else str(val)
 
 #funkcja do zapisu dzielenia przez liczbę z możliwym znakiem +/-
 def divided_by_val_with_sign(val):
     if val < 0:
         return f"({val})"       #jeśli dzielnik jest ujemny to zapis wyglada np. (-27)
-    else: return str(val)       #jeśli dodatni to bez zmian
+    else: 
+        return str(val)       #jeśli dodatni to bez zmian
 
 # losowo przyznaje znak liczbie 
 def draw_value_sign_randomly(val):
-    draw = r.randint(0, 1)
-    if draw < 1:
-        val = -val
-    return val
+    return -val if r.randint(0, 1) == 0 else val # nowy, lepszy skrócony zapis + jest bardziej czytelny
 
 '''<----> ZADANIE ARYTMETYCZNE <---->'''
 
@@ -68,7 +61,7 @@ def generate_arithmetic_task(level):
             a = r.randint(1 * 5**(level-1) , 10 * 5**(level-1))
             b = r.randint(1 * 5**(level-1) , 10 * 5**(level-1))
 
-    while idx == 3 and a%b!=0:
+    while idx == 3 and (b == 0 or a % b != 0):
         a = r.randint(1 * 5**(level-1) , div_max[level-1])
         b = r.randint(1 * 5**(level-1) , div_max[level-1])
         if b > a:
@@ -90,13 +83,6 @@ def generate_arithmetic_task(level):
 
     return question, answer
 
-# TESTY FUNKCJI generate_arithmetic_task()
-
-print(f"Zadanie arytmetyczne 1 poziomu: {generate_arithmetic_task(1)}")
-print(f"Zadanie arytmetyczne 2 poziomu: {generate_arithmetic_task(2)}")
-print(f"Zadanie arytmetyczne 3 poziomu: {generate_arithmetic_task(3)}")
-
-
 '''<----> ZADANIE ALGEBRAICZNE PROSTE <---->'''
 
 # zakresy wartości współczynników w prostych rówaniach z niewiadomą "x"
@@ -112,7 +98,7 @@ def simple_equation_gen_ranges(level):
     
     # zakres dla drugiego poziomu
     second_range = {
-        "a": (2, 6),
+        "a": (1, 6),
         "b": (2, 20),
         "x": (2, 10),
         "d": (5, 50),
@@ -126,7 +112,7 @@ def simple_equation_gen_ranges(level):
         "d": (10, 100), 
         "y": (1, 20)}
 
-    # zwrócenie zakresu w  zależności od poziomu
+    # zwrócenie zakresu w zależności od poziomu
     match level:
         case 1:
             return first_range
@@ -134,6 +120,8 @@ def simple_equation_gen_ranges(level):
             return second_range
         case 3:
             return third_range
+        case _:
+            return first_range if not str(level).isnumeric() else first_range if level < 1 else third_range
 
 # generowanie zadania typu pierwszego
 def gen_type_one(level): # ax + b = y
@@ -158,13 +146,17 @@ def gen_type_one(level): # ax + b = y
 def gen_type_two(level): # ax + b = cx + y
 
     ranges = simple_equation_gen_ranges(level)
-    a, b, c, x = r.randint(*ranges["a"]), r.randint(*ranges["b"]), r.choice([r.randint(*ranges["d"]), r.randint(*ranges["x"])]), r.randint(*ranges["x"])
-
-    while a == c:
-        c = r.choice([r.randint(*ranges["d"]), r.randint(*ranges["x"])])
+    a, b, c, x = r.randint(*ranges["a"]), r.randint(*ranges["b"]), r.randint(*ranges["d"]) if r.randint(0,1) else r.randint(*ranges["x"]), r.randint(*ranges["x"])
     
     if level > 1:
             a, b, c, x = draw_value_sign_randomly(a), draw_value_sign_randomly(b), draw_value_sign_randomly(c), draw_value_sign_randomly(x)
+
+    for _ in range(25):
+        if a != c:
+            break
+        c = r.randint(*ranges["d"]) if r.randint(0,1) else r.randint(*ranges["x"])
+    else:
+        c += 1
 
     y = (a - c) * x + b
  
@@ -209,14 +201,89 @@ def generate_basic_equation_task(level):
 
     level = level_clamp(level) 
 
-    #match level: # tu mam zamiar napisać instrukcję, która sprawi, że pod wpływem poziomu zmieniają się szanse na poszczególny typ zadania algebraicznego prostego
+    # tu mam zamiar napisać instrukcję, która sprawi, że pod wpływem poziomu zmieniają się szanse na poszczególny typ zadania algebraicznego prostego
+    match level:
+        case 1:
+            chances = [70, 20, 10] # typ 1: 70%, typ 2: 20%, typ 3: 10%
+        case 2:
+            chances = [30, 50, 20]
+        case 3:
+            chances = [10, 20, 70]
 
-    question, answer = r.choice([gen_type_one(level), gen_type_two(level), gen_type_three(level)]) #gen_type_three(level) #gen_type_one(level), gen_type_two(level),
+    generators = [gen_type_one, gen_type_two, gen_type_three]
+    result = r.choices(generators, weights = chances, k = 1)[0] #gen_type_three(level) #gen_type_one(level), gen_type_two(level)
 
+    # random choices działa trochę inaczej od choice - zwraca krotkę zamiast dwóch wartości
+    question, answer = result(level)
+    
     return question, answer
 
-# TESTY FUNKCJI generate_basic_equation_task()
+# funkcja do testowania generowania zadań
+def gen_test():
 
-print(f"Zadanie algebraiczne proste 1 poziomu: {generate_basic_equation_task(1)}")
-print(f"Zadanie algebraiczne proste 2 poziomu: {generate_basic_equation_task(2)}")
-print(f"Zadanie algebraiczne proste 3 poziomu: {generate_basic_equation_task(3)}")  
+    def functions_test_handling(function, type, quantity):
+        for x in range(quantity):
+            print(f"\nTEST NR. {x+1}")
+            for i in range(1, 4):
+                    qstn, ans = function(i)
+                    print(f"Zadanie typu {type.upper()} ({i} poziomu): {qstn}\t\tPoprawna odpowiedź: {ans}")
+        print()
+
+    def tests_quantity():
+        number_of_tests = input("Zadeklaruj ilość testów (max 20): \n> ")
+        if not number_of_tests.isnumeric():
+            number_of_tests = 1
+        elif number_of_tests.isnumeric():
+            number_of_tests = int(number_of_tests)
+            if number_of_tests < 1 or number_of_tests > 20:
+                number_of_tests = 1
+        return number_of_tests
+            
+
+    print("TESTY FUNKCJI")
+    list_of_choices = enumerate(TASK_TYPES) #ponumerowana lista wyborów
+    for number, task in list_of_choices:
+        print(f"{number+1} - {task}")
+    print("x - EXIT")
+    choice = input("Podaj numer funkcji do przetestowania: \n> ")
+    if choice.isnumeric():
+        choice = int(choice)
+        if choice in range(1, len(TASK_TYPES)+1):
+            chosen_type = TASK_TYPES[choice - 1]
+        else:
+            print("Funkcja niedostępna lub nie istnieje!")
+
+    # w zależności od wyboru pokazuje odpowiadające testy
+    match choice:
+
+        case 1:
+              # TESTY FUNKCJI generate_arithmetic_task()
+            print()
+            print("TEST FUNKCJI generate_arithmetic_task() -> Działania arytmetyczne")
+            print()
+            functions_test_handling(generate_arithmetic_task, chosen_type, tests_quantity())
+            
+        case 2:
+            # TESTY FUNKCJI generate_basic_equation_task()
+            print()
+            print("TEST FUNKCJI generate_basic_equation_task() -> Działania algebraiczne proste")
+            print()
+            functions_test_handling(generate_basic_equation_task, chosen_type, tests_quantity())
+        
+        # zakończenie testów
+        case "x":
+            print()
+            print("ZAKOŃCZONO TESTY")
+            print()
+            exit()
+            
+        case _: #stan domyślny
+            print()
+            print("Funkcja niedostępna lub nie istnieje!")
+            print()
+
+# zapobiegnięcie wywoływania testów kiedy moduł jest importowany
+if __name__ == "__main__":
+    while True:
+        gen_test()
+        input("Wciśnij ENTER by kontynuować... \n")
